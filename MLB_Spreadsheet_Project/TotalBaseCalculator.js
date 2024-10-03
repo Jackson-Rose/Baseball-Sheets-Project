6aa28d8995c32350d4a83a1820762e7a63c351ef
Jackson Rose <33973334+Jackson-Rose@users.noreply.github.com>
2024-10-03 15:47:46 -0700

    Update TotalBaseCalculator.js

--- MLB_Spreadsheet_Project/TotalBaseCalculator.js
+++ MLB_Spreadsheet_Project/TotalBaseCalculator.js
@@ -1,12 +1,39 @@
-function RBI_Calculator(inputText) {
+function TBC(inputText) {
 var textToSearch = inputText.toString(); 
-var regexRBI = /(\d*)\s*RBI/g;
-var match = regexRBI.exec(textToSearch);
+var total_bases = textToSearch[0];
+
+var regex2B = /(\d*)\s*2B/g;
+var match = regex2B.exec(textToSearch);
 if (match !== null) {
     var xValue = match !== null && match[1] !== "" ? parseInt(match[1]) : 1;
+    var result = 2 * xValue
+    total_bases = Number(total_bases) + Number(result); 
+    total_bases = total_bases - Number(xValue);
+} else {
+   
 }
-else {
-  xValue = 0
+
+var regex3B = /(\d*)\s*3B/g;
+match = regex3B.exec(textToSearch);
+if (match !== null) {
+    var xValue = match !== null && match[1] !== "" ? parseInt(match[1]) : 1;
+    var result = 3 * xValue
+    total_bases = Number(total_bases) + Number(result); 
+    total_bases = total_bases - Number(xValue);
+} else {
+    
 }
-return(xValue)
+
+var regexHR = /(\d*)\s*HR/g;
+match = regexHR.exec(textToSearch);
+if (match !== null) {
+    var xValue = match !== null && match[1] !== "" ? parseInt(match[1]) : 1;
+    var result = 4 * xValue
+    total_bases = Number(total_bases) + Number(result); 
+    total_bases = total_bases - Number(xValue);
+} else {
+    
+}
+
+return(total_bases);
 }
